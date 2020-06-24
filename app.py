ac2efc0cd5cc06d6ec45ecf1686b78423c4d8903
Daniel Chavez <danielchavez@uchicago.edu>
2020-06-23 22:42:37 -0400

    removed additions

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask_migrate import Migrate
 from app import create_app, db
-from flask import Flask
 
 app = create_app()
-app = Flask(__name__)
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
     return(dict(db=db))
 
-if __name__ == '__main__':
-    app.run()
+
