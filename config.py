866a439cb9feef6dd3e5da549a641b6bc726e9c3
Daniel Chavez <danielchavez@uchicago.edu>
2020-06-23 22:47:06 -0400

    prop exceptions

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ClothingDB.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_ADMIN = 'daniel.chavez9797@gmail.com'
+    PROPOGATE_EXCEPTIONS = True
