19715e7f8861dd1eae8bb07ff7b87d545b9cac97
Daniel Chavez <danielchavez@uchicago.edu>
2020-06-27 23:04:23 -0400

    templating issues fixed

--- app/static/js/new_item.js
+++ app/static/js/new_item.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 $("#itemBrand").select2({
     minimumInputLength: 1,
     ajax: {
-        url: 'http://127.0.0.1:5000/api/Brand',
+        url: "{{url_for('api.get_brands')}}",
         dataType: 'json',
         type: "GET",
         quietMillis: 50,
@@ -21,7 +21,7 @@ $("#itemBrand").select2({
 $("#itemStyle").select2({
     minimumInputLength: 1,
     ajax: {
-        url: 'http://127.0.0.1:5000/api/Style',
+        url: "{{url_for('api.get_styles')}}",
         dataType: 'json',
         type: "GET",
         quietMillis: 50,
