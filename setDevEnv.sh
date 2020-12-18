2046e5d64f07f5b68a2ac7eb0efc4aa1858f6924
Daniel Chavez <daniel.chavez9797@gmail.com>
2020-12-17 21:32:57 -0500

    removed files

--- setDevEnv.sh
+++
@@ -1,6 +0,0 @@
-source venv/bin/activate
-export FLASK_APP=clothdb.py
-export FLASK_DEBUG=1
-export ELASTICSEARCH_URL="http://elastic:daniel97@34.198.0.244:9200"
-export DEV_DATABASE_URL="postgresql://danielchavez:daniel97@ec2-34-198-0-244.compute-1.amazonaws.com/ClothDB_DEV"
-export FLASK_CONFIG="development"
--- setProdEnv.sh
+++
@@ -1,5 +0,0 @@
-export FLASK_APP=clothdb.py
-export FLASK_DEBUG=1
-export ELASTICSEARCH_URL="http://elastic:daniel97@34.198.0.244:9200"
-export DATABASE_URL="postgresql://danielchavez:daniel97@ec2-34-198-0-244.compute-1.amazonaws.com/ClothDB_PROD"
-export FLASK_CONFIG="production"
